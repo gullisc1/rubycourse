146f715de84b76498647327a3285d9d2ae5616cf
Guglielmo Schenardi <guglielmoschenardi@yahoo.it>
2020-08-13 19:23:53 +0200

    Show user's artciles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def gravatar_for(user, option = {size: 80})
+    email_addres = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_addres)
+    size = option[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+  end 
 end
